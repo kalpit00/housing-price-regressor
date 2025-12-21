990bbbdca30f30c22e8f02f650df3fdfd3748fd5
kalpit00 <kalpit.k.patel@gmail.com>
2025-12-21 17:04:24 -0500

    added .data dir to gitIgnore large files

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def main():
-    print("Hello from housing-regression-mle!")
-
+    print("Hi, my name is Kalpit. This is my first ML project. It's a housing price regressor!")
 
 if __name__ == "__main__":
     main()
